2249ea9e0c3f2e19167e57da90c07076fd48a6e0
Takkakynttila <42032491+Takkakynttila@users.noreply.github.com>
2021-08-25 23:43:02 +0300

    Added more functionality

--- main.py
+++ main.py
@@ -2,32 +2,30 @@ from functions import data_bank
 
 def main():
     # promt the user for parameters
-    length = input('Give desired word length: ')
+    length = input('Give desired word length (0 <= n <= 8): ')
+    if int(length) > 8:
+        print('Invalid')
+        quit()
     char_set = input('Choose character set: \n'
     + '[1] ASCII\n'
-    + '[2] Lower case alphabet \n>>> ')
+    + '[2] Lower case alphabet \n'
+    + '[3] Test set \n>>> ')
 
     #initialize an object based on parameters
     f = data_bank(length, char_set)
 
     # flex with some calculations
-    print('Amount of permutations: ' + str(f.calculate_permutations()))
+    print('Amount of permutations: ' + str(f.calculate_combinations()))
     print('Approx filesize: \n'
-    + str(f.calculate_size_approximation()) + ' bytes \n'
-    + str(f.calculate_size_approximation() / 1000000) + ' megabytes')
+    + '[+]' + str(f.calculate_size_approximation()) + ' bytes \n'
+    + '[+]' + str(f.calculate_size_approximation() / 1000000) + ' megabytes')
     response = input('Create strings? y/n \n>>> ')
 
     if response.lower() == 'y':
-        x = 0
-        word_as_list = [f.get_char(0)] * int(length)
-        char_list = f.get_char_list()
-        while x < int(length):
-            for char in word_as_list:
-                for char in char_list:
-                    word_as_list[x] = char
-                    print(str(word_as_list))
-            x += 1
+        f.get_combinations(length)
     else:
-        print('Aborting creation!')
+        print('Aborting creation')
 if __name__ == '__main__':
     main()
+
+
